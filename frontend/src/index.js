182e4c22aa9e6ea55f1f896eea592a978c10ac8b
RMS <rene.as.himself@gmail.com>
2019-06-01 17:58:24 -0500

    Netlify venv changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,7 +19,8 @@ console.log(`END_POINT: ${process.env.END_POINT}`)
 
 // Apollo Boost
 const client = new ApolloClient({
-    uri: process.env.END_POINT || 'http://localhost:4444/',
+    uri:
+        process.env.NODE_ENV === 'dev' ? 'http://localhost:4444/' : process.env.REACT_APP_END_POINT,
     request: async operation => {
         operation.setContext({
             fetchOptions: {
