025989bc8737d252f1d5bf1a9f6d0c632090ed52
RMS <rene.as.himself@gmail.com>
2019-07-01 10:32:50 -0500

    Particles on homepage

--- frontend/src/components/Layout/Home.js
+++ frontend/src/components/Layout/Home.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import Particles from 'react-particles-js'
+import styled from 'styled-components'
 import Main from './Main'
 
+const particlesOptions = {
+    particles: {
+        number: {
+            value: 50,
+            density: {
+                enable: true,
+                value_area: 800,
+            },
+        },
+    },
+}
+
 const Home = () => (
-    <Main>
-        <h1>Cero</h1>
-        <h2>Cero is a simple intermittent fasting tracker app</h2>
-    </Main>
+    <>
+        <Particles className='particles' params={particlesOptions} />
+        <Main>
+            <h1>Cero</h1>
+            <h2>Cero is a simple intermittent fasting tracker app</h2>
+        </Main>
+    </>
 )
 
 export default Home
