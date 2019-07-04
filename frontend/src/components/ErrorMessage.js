a89874f0aadc47efb96494a7ba49c2fc55012189
RMS <rene.as.himself@gmail.com>
2019-07-04 15:40:50 -0500

    Subscription mutation

--- frontend/src/components/ErrorMessage.js
+++ frontend/src/components/ErrorMessage.js
@@ -8,10 +8,12 @@ const ErrorStyles = styled.div`
     margin: 2rem 0;
     border: 1px solid rgba(0, 0, 0, 0.05);
     border-left: 5px solid red;
+
     p {
         margin: 0;
         font-weight: 100;
     }
+
     strong {
         margin-right: 1rem;
     }
