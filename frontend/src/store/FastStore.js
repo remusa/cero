bd7b37306b38e53446e13d67db693785a27da904
RMS <rene.as.himself@gmail.com>
2019-09-30 12:59:06 -0500

    timeConversion tests

---
+++ frontend/src/store/FastStore.js
@@ -0,0 +1,19 @@
+// import dayjs from 'dayjs'
+// import {action, computed, observable} from 'mobx'
+// import {persist} from 'mobx-persist'
+
+// export class FastTimerStore {
+//     @persist('object') @observable startTime = dayjs()
+//     @persist @observable isRunning = false
+//     @persist @observable seconds = 0
+
+//     @action measure() {
+//         if (!this.isRunning) return
+
+//         this.seconds = dayjs().diff(this.startTime, 'second')
+
+//         setTimeout(() => {
+
+//         }, this.measure(), 1000);
+//     }
+// }
