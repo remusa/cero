ddd384b541409389e9dffb904ff41d04015633ae
RMS <rene.as.himself@gmail.com>
2019-09-30 13:11:03 -0500

    Commented serviceWorker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloProvider as ApolloProviderHooks } from '@apollo/react-hooks'
 import { BrowserRouter as Router } from 'react-router-dom'
-import * as serviceWorker from './serviceWorker'
+// import * as serviceWorker from './serviceWorker'
 import App from './App'
 
 require('dotenv').config({ path: '.env' })
@@ -43,4 +43,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// serviceWorker.unregister()
