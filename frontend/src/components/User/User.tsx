6889d7690ee16dc827f97999d0a53c3f28908dec
RMS <rene.as.himself@gmail.com>
2019-10-02 18:00:04 -0500

    Refactored User to TS

---
+++ frontend/src/components/User/User.tsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Query, QueryResult } from 'react-apollo'
+import { CURRENT_USER_QUERY } from '../../gql/UserQuery'
+import { JSXElement } from '@babel/types'
+
+interface Props {
+    props: {
+        children: React.ReactNode | null
+    }
+}
+
+const User: React.FC<Props> = props => (
+    <Query {...props} query={CURRENT_USER_QUERY}>
+        {(payload: QueryResult<any, Record<string, any>>) => props.children(payload)}
+    </Query>
+)
+
+// User.propTypes = {
+//     children: PropTypes.func.isRequired,
+// }
+
+export default User
