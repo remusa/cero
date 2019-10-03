0944ecb623f992efa627abfe02d0189e3696461d
RMS <rene.as.himself@gmail.com>
2019-10-02 20:10:06 -0500

    Added User interface

--- frontend/src/data/UserContext.tsx
+++ frontend/src/data/UserContext.tsx
@@ -1,18 +1,26 @@
 import React, { createContext, Dispatch, SetStateAction, useState } from 'react'
 
-interface IContext {
-    user: object
+interface IUser {
+    id: string
+    email: string
+    goal: number
+    name: string
+    permissions: string[]
+}
+
+interface IUserContext {
+    user: IUser
     setUser: Dispatch<SetStateAction<object>>
 }
 
-const UserContext = createContext({} as IContext)
+const UserContext = createContext({} as IUserContext)
 
 interface Props {
     children: HTMLElement
 }
 
 const UserProvider: React.FC<Props> = ({ children }) => {
-    const [user, setUser] = useState<object>({})
+    const [user, setUser] = useState<IUser>({})
 
     return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
 }
