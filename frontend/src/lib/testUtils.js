c3b15643090cc5a9ce2bd14a6a80cfdebdeb7165
RMS <rene.as.himself@gmail.com>
2019-10-03 03:57:58 -0500

    Fixed Jest configuration to run Enzyme/react-testing-library tests

--- frontend/src/lib/testUtils.js
+++ frontend/src/lib/testUtils.js
@@ -1,79 +1,75 @@
-import casual from 'casual';
+import casual from 'casual'
 
-casual.seed(777);
+casual.seed(777)
 
-const fakeItem = () => ({
-  __typename: 'Item',
-  id: 'abc123',
-  price: 5000,
-  user: null,
-  image: 'dog-small.jpg',
-  title: 'dogs are best',
-  description: 'dogs',
-  largeImage: 'dog.jpg',
-});
+export const fakeUser = () => ({
+    __typename: 'User',
+    id: '1234',
+    name: casual.name,
+    email: casual.email,
+    goal: 14,
+    permissions: ['ADMIN']
+})
 
-const fakeUser = () => ({
-  __typename: 'User',
-  id: '4234',
-  name: casual.name,
-  email: casual.email,
-  permissions: ['ADMIN'],
-  orders: [],
-  cart: [],
-});
+export const fakeItem = () => ({
+    __typename: 'Item',
+    id: 'abc123',
+    price: 5000,
+    user: null,
+    image: 'dog-small.jpg',
+    title: 'dogs are best',
+    description: 'dogs',
+    largeImage: 'dog.jpg',
+})
 
-const fakeOrderItem = () => ({
-  __typename: 'OrderItem',
-  id: casual.uuid,
-  image: `${casual.word}.jpg`,
-  title: casual.words(),
-  price: 4234,
-  quantity: 1,
-  description: casual.words(),
-});
+export const fakeOrderItem = () => ({
+    __typename: 'OrderItem',
+    id: casual.uuid,
+    image: `${casual.word}.jpg`,
+    title: casual.words(),
+    price: 4234,
+    quantity: 1,
+    description: casual.words(),
+})
 
-const fakeOrder = () => ({
-  __typename: 'Order',
-  id: 'ord123',
-  charge: 'ch_123',
-  total: 40000,
-  items: [fakeOrderItem(), fakeOrderItem()],
-  createdAt: '2018-04 - 06T19: 24: 16.000Z',
-  user: fakeUser(),
-});
+export const fakeOrder = () => ({
+    __typename: 'Order',
+    id: 'ord123',
+    charge: 'ch_123',
+    total: 40000,
+    items: [fakeOrderItem(), fakeOrderItem()],
+    createdAt: '2018-04 - 06T19: 24: 16.000Z',
+    user: fakeUser(),
+})
 
-const fakeCartItem = overrides => ({
-  __typename: 'CartItem',
-  id: 'omg123',
-  quantity: 3,
-  item: fakeItem(),
-  user: fakeUser(),
-  ...overrides,
-});
+export const fakeCartItem = overrides => ({
+    __typename: 'CartItem',
+    id: 'omg123',
+    quantity: 3,
+    item: fakeItem(),
+    user: fakeUser(),
+    ...overrides,
+})
 
 // Fake LocalStorage
-class LocalStorageMock {
-  constructor() {
-    this.store = {};
-  }
+export class LocalStorageMock {
+    constructor() {
+        this.store = {}
+    }
 
-  clear() {
-    this.store = {};
-  }
+    clear() {
+        this.store = {}
+    }
 
-  getItem(key) {
-    return this.store[key] || null;
-  }
+    getItem(key) {
+        return this.store[key] || null
+    }
 
-  setItem(key, value) {
-    this.store[key] = value.toString();
-  }
+    setItem(key, value) {
+        this.store[key] = value.toString()
+    }
 
-  removeItem(key) {
-    delete this.store[key];
-  }
+    removeItem(key) {
+        delete this.store[key]
+    }
 }
-
-export { LocalStorageMock, fakeItem, fakeUser, fakeCartItem, fakeOrder, fakeOrderItem, };
-
