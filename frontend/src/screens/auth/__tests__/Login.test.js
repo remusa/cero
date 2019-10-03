12f586e46766c3a053662196fab87f303fec5cce
RMS <rene.as.himself@gmail.com>
2019-10-03 15:44:14 -0500

    Update Login.test.js

--- frontend/src/screens/auth/__tests__/Login.test.js
+++ frontend/src/screens/auth/__tests__/Login.test.js
@@ -1,16 +1,45 @@
-import React
-import { render, fireEvent, queryByText, cleanup } from "@testing-library/react";
+import React from 'react'
+import { render, fireEvent, queryByText, cleanup } from '@testing-library/react'
 import Login from '../Login'
-// import * as jest from 'jest'
+import { CURRENT_USER_QUERY } from './../../../gql/UserQuery'
+import { fakeUser } from './../../../lib/testUtils'
+import { MockedProvider } from '@apollo/react-testing'
 
-// afterEach(cleanup)
+afterEach(cleanup)
 
-it('calls onSubmit with email/password when submitted', () => {
+const notSignedInMocks = [
+    {
+        request: { query: CURRENT_USER_QUERY },
+        result: { data: { me: null } },
+    },
+]
+
+const signedInMocks = [
+    {
+        request: { query: CURRENT_USER_QUERY },
+        result: { data: { me: fakeUser() } },
+    },
+]
+
+it.only('should render the minimal header when logged out', async () => {
+    const { getByText, container } = render(
+        <MockedProvider mocks={notSignedInMocks} addTypename={false}>
+            <Login />
+        </MockedProvider>
+    )
+
+    await wait()
+
+    const nav = getByText('navigation')
+    expect(container).toMatchSnapshot()
+})
+
+it.skip('calls onSubmit with email/password when submitted', () => {
     // Arrange
-    const fakeUser = {email: 'test@test.com', password: '12345qwerty'}
+    const fakeUser = { email: 'test@test.com', password: '12345qwerty' }
     const handleSubmit = jest.fn()
 
-    const {getByLabelText, getByText} = render(<Login />)
+    const { getByLabelText, getByText } = render(<Login />)
 
     const emailNode = getByLabelText(/email/i)
     const passwordNode = getByLabelText(/password/i)
@@ -24,4 +53,4 @@ it('calls onSubmit with email/password when submitted', () => {
     // Assert
     expect(handleSubmit).toHaveBeenCalledTimes(1)
     expect(handleSubmit).toHaveBeenCalledWith(fakeUser)
-});
+})
