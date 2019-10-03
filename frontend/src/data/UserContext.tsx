c5b62b5844e2620aaab54ed939d9c4056b312738
RMS <rene.as.himself@gmail.com>
2019-10-02 20:11:54 -0500

    Update UserContext.tsx

--- frontend/src/data/UserContext.tsx
+++ frontend/src/data/UserContext.tsx
@@ -10,7 +10,7 @@ interface IUser {
 
 interface IUserContext {
     user: IUser
-    setUser: Dispatch<SetStateAction<object>>
+    setUser: Dispatch<SetStateAction<IUser>>
 }
 
 const UserContext = createContext({} as IUserContext)
@@ -20,9 +20,16 @@ interface Props {
 }
 
 const UserProvider: React.FC<Props> = ({ children }) => {
-    const [user, setUser] = useState<IUser>({})
+    const [user, setUser] = useState<IUser>({
+        id: '',
+        email: '',
+        goal: 0,
+        name: '',
+        permissions: [],
+    })
 
     return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
 }
 
 export { UserContext, UserProvider }
+
