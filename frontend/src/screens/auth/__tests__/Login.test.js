3417ae76bd71c1bacff63685bec67120f91314d3
RMS <rene.as.himself@gmail.com>
2019-10-04 14:20:05 -0500

    Revert "Fixed TS error when comparing"
    This reverts commit 6940c46e732018e7f66dd65909c6ab5c1bcaa919.

--- frontend/src/screens/auth/__tests__/Login.test.js
+++ frontend/src/screens/auth/__tests__/Login.test.js
@@ -1,45 +1,16 @@
-import React from 'react'
-import { render, fireEvent, queryByText, cleanup } from '@testing-library/react'
+import React
+import { render, fireEvent, queryByText, cleanup } from "@testing-library/react";
 import Login from '../Login'
-import { CURRENT_USER_QUERY } from './../../../gql/UserQuery'
-import { fakeUser } from './../../../lib/testUtils'
-import { MockedProvider } from '@apollo/react-testing'
+// import * as jest from 'jest'
 
-afterEach(cleanup)
+// afterEach(cleanup)
 
-const notSignedInMocks = [
-    {
-        request: { query: CURRENT_USER_QUERY },
-        result: { data: { me: null } },
-    },
-]
-
-const signedInMocks = [
-    {
-        request: { query: CURRENT_USER_QUERY },
-        result: { data: { me: fakeUser() } },
-    },
-]
-
-it.only('should render the minimal header when logged out', async () => {
-    const { getByText, container } = render(
-        <MockedProvider mocks={notSignedInMocks} addTypename={false}>
-            <Login />
-        </MockedProvider>
-    )
-
-    await wait()
-
-    const nav = getByText('navigation')
-    expect(container).toMatchSnapshot()
-})
-
-it.skip('calls onSubmit with email/password when submitted', () => {
+it('calls onSubmit with email/password when submitted', () => {
     // Arrange
-    const fakeUser = { email: 'test@test.com', password: '12345qwerty' }
+    const fakeUser = {email: 'test@test.com', password: '12345qwerty'}
     const handleSubmit = jest.fn()
 
-    const { getByLabelText, getByText } = render(<Login />)
+    const {getByLabelText, getByText} = render(<Login />)
 
     const emailNode = getByLabelText(/email/i)
     const passwordNode = getByLabelText(/password/i)
@@ -53,4 +24,4 @@ it.skip('calls onSubmit with email/password when submitted', () => {
     // Assert
     expect(handleSubmit).toHaveBeenCalledTimes(1)
     expect(handleSubmit).toHaveBeenCalledWith(fakeUser)
-})
+});
